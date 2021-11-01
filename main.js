e10592eebdf7e3427623fbc687941ade2c25ed23
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2021-10-31 20:39:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ noseX=0;
 noseY=0;
 
 function preload(){
+    mustache = loadImage('https://i.postimg.cc/ZnSfnjX1/Mustache-P114-116.png');
 }
 
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+
     video = createCapture(VIDEO);
     video.size(300,300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet Is Initialized');
 }
 
 function gotPoses(results)
@@ -27,12 +33,10 @@ function gotPoses(results)
     }
 }
 
-function modelLoaded(){
-    console.log('poseNet Is Initialized');
-}
 
 function draw(){
     image(video,0,0,300,300);
+    image(mustache, noseX, noseY,30,30);
 }
 
 function take_snapshot(){
